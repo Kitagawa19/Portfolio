a72555b5e78dda32f5e7335db059f37f51976bea
Kitagawa19 <kkitagawa16219@gmail.com>
2024-09-01 22:10:01 +0900

    追加 ナビゲーションバー

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,9 +1,34 @@
+'use client';
 import React from 'react';
+import { AppBar, Toolbar, Typography, Box, IconButton, Button } from '@mui/material';
+import { ThemeProvider,createTheme } from '@mui/material/styles';
+import  MenuIcon  from '@mui/icons-material/Menu';
+import theme from '@/theme';
 
 export const Header = () =>{
     return(
-        <div>
-            
-        </div>
+        <>
+            <ThemeProvider theme={createTheme(theme)}>
+                <Box sx={{ flexGrow: 1 }}>
+                    <AppBar position="static">
+                        <Toolbar>
+                            <IconButton
+                              size="large"
+                              edge="start"
+                              color="inherit"
+                              aria-label="menu"
+                              sx={{ mr: 2 }}
+                            >
+                              <MenuIcon />
+                            </IconButton>
+                            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                                ポートフォリオサイト
+                            </Typography>
+                            <Button color="inherit">Login</Button>
+                        </Toolbar>
+                    </AppBar>
+                </Box>
+            </ThemeProvider>
+        </>
     )
 }
--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,13 @@
-import { Noto_Sans_JP } from '@next/font/google';
+import { Noto_Sans_JP } from 'next/font/google';
 
-const noto_sans_jp= Noto_Sans_JP({
-    weights: [ 300, 400, 500, 700],
-    subsets: ['latin', 'japanese'],
+const notosansjp= Noto_Sans_JP({
+    weight: ['300', '400', '500', '700'],
+    subsets: ['latin'],
     display: 'swap',
 });
 const theme = {
-    typegraphy: {
-        fontFamily: 'Noto Sans JP, sans-serif',
+    typography: {
+        fontFamily: notosansjp.style.fontFamily,
     }
 }
 export default theme;
