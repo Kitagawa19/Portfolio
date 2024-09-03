33cf34134630f78f27ead26498452570d18e4c8e
KitagawaKouta <kkitagawa16219@gmail.com>
2024-09-03 18:02:36 +0900

    追加 rader chart

---
+++ src/Components/parts/skillGraph.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Radar } from "react-chartjs-2";
+import {
+  Chart as ChartJS,
+  RadialLinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register()
