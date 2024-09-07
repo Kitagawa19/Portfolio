76226772c824746a97dab32c08c9e0845212ecb6
KitagawaKouta <kkitagawa16219@gmail.com>
2024-09-07 16:39:46 +0900

    追加 myinfo

---
+++ src/Components/parts/myInfo.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Typography, Box, Container } from "@mui/material";
+
+export const MyProfile:React.FC = () => {
+  return (
+    <Container>
+      <Typography variant="h3"> My Profile</Typography>
+      
+    </Container>
+  )
+}
