cc6af1f5a95fcab7d59ece02436747a12715d8f8
Kitagawa19 <kkitagawa16219@gmail.com>
2024-09-22 03:57:52 +0900

    変更 blog type

--- src/types/Blog.ts
+++ src/types/Blog.ts
@@ -1,13 +1,10 @@
-import { MicroCMSImage, MicroCMSListContent, MicroCMSListResponse } from 'microcms-js-sdk'
-
-import { Category } from './Category'
-
-export type Blog = {
-  title: string
-  category: Category & MicroCMSListContent
-  toc_visible: boolean
-  body: string
-  description: string
-  ogimage: MicroCMSImage
-  related_blogs: MicroCMSListResponse<Blog>['contents']
+export type Post = {
+  id: string;
+  title: string;
+  eyecatch?: {
+    url: string;
+  }
+  category?: {
+    name: string;
+  };
 }
