6c42515bd92bf5be541c81eca2623c5213c2abd5
Kitagawa19 <kkitagawa16219@gmail.com>
2024-09-21 22:40:14 +0900

    編集 microCMS client component

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -1,6 +1,22 @@
 import { createClient } from 'microcms-js-sdk';
 
 export const client = createClient({
-  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN as string,
-  apiKey: process.env.MICROCMS_API_KEY as string,
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN || '',
+  apiKey: process.env.MICROCMS_API_KEY || '',
 });
+
+export async function getArticles() {
+  const response = await client.get({
+    endpoint: 'articles',
+    queries: {orders: '-publishedAt'}
+  })
+  return response.contents
+}
+
+export async function getArticle(id: string){
+  const response = await client.get({
+    endpoint: 'articles',
+    contentId: id
+  })
+  return response
+}
