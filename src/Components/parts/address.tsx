af7125dd135f061c10ff672ca0f7989680fc51b5
KitagawaKouta <kkitagawa16219@gmail.com>
2024-10-02 16:54:56 +0900

    add email info

--- src/Components/parts/address.tsx
+++ src/Components/parts/address.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Typography, Box, Container,List,ListItem,ListItemIcon,ListItemText } from "@mui/material";
 import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
+
 import Link from "next/link";
 
 export const Address:React.FC = () => {
@@ -17,6 +19,14 @@ export const Address:React.FC = () => {
               Github:
               <Link href="https://github.com/Kitagawa19" target="_blank" rel="noopener noreferrer">マイアカウント</Link>
             </ListItemText>
+            <ListItem>
+              <ListItemIcon>
+                <EmailIcon />
+              </ListItemIcon>
+              <ListItemText>
+                Email： ktc2243020001@edu.kyoto-tech.ac.jp
+              </ListItemText>
+            </ListItem>
         </List>
       </Container>
     )
