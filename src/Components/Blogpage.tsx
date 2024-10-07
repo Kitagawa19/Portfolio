4dccbe7a07aa3078e30a8ab8974d4bd1764526c0
KitagawaKouta <kkitagawa16219@gmail.com>
2024-10-07 11:34:03 +0900

    fix マイクロCMSから画像を

--- src/Components/Blogpage.tsx
+++ src/Components/Blogpage.tsx
@@ -7,28 +7,29 @@ import Image from "next/image";
 
 export default async function BlogPage() {
   const { contents } = await getBlogList();
+
   return (
     <Container>
       <Box>
         {contents.map((post: Blog) => (
           <Box key={post.id} mb={4}>
-            {post.eyecatch && (
+            {post.eyecatch?.url && (
               <Box mt={2}>
                 <Image
-                  src={post.eyecatch.url} 
-                  alt={post.title}
-                  width={post.eyecatch.width} 
-                  height={post.eyecatch.height} 
+                  src={post.eyecatch.url}
+                  alt={post.title || "No title"}
+                  width={post.eyecatch.width || 500}
+                  height={post.eyecatch.height || 300}
                 />
-                <Typography variant="h4">
-                  <Link href={`/blog/${post.id}`}>
-                    {post.title}
-                  </Link>
-                </Typography>
               </Box>
             )}
+            <Typography variant="h4">
+              <Link href={`/blogs/${post.id}`}>
+                {post.title || "Untitled"}
+              </Link>
+            </Typography>
             <Typography variant="body2" color="textSecondary">
-              Category: {post.category || "Uncategorized"}
+              Category: {post.category?.name || "Uncategorized"}
             </Typography>
           </Box>
         ))}
