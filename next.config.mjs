5a9847c488c6c2be8cf4f0e9efde01a58eb8126f
KitagawaKouta <kkitagawa16219@gmail.com>
2024-10-30 22:24:37 +0900

    update setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,28 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns:[
+    remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.microcms-assets.io'
-      }
-    ]
+        hostname: 'images.microcms-assets.io',
+      },
+    ],
+  },
+  // ISRやキャッシュに関する設定
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // 全ページに対するキャッシュ制御
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, must-revalidate', // キャッシュ無効化設定
+          },
+        ],
+      },
+    ];
   },
 };
 
